808f5ae4f50307a61ba9dd98d4c8d25374f61b14
Ben Wainwright <ben.wainwright@cinch.co.uk>
2023-11-04 00:59:23 +0000

    docs: add example to docs

--- src/lib/core/initialise-api.ts
+++ src/lib/core/initialise-api.ts
@@ -5,6 +5,19 @@ import { RememberTheMilkApi } from "../remember-the-milk-api";
 /**
  * Entry point to the API. Calling it with valid credentials will initialise and return an instantiated {@link IRememberTheMilkApi}
  *
+ * @example
+ * ```TypeScript
+ * import { initialiseApi, ClientPermissions } from "rtm-typescript"
+ *
+ * // ...some code
+ *
+ * const client = initialiseApi("my-key", "my-secret", ClientPermissions.Read)
+ *
+ * const { frob } = await client.auth.getFrob()
+ *
+ * console.log(frob)
+ * ```
+ *
  * @param key - Remember the Milk API key
  * @param secret - Remember the Milk API shared secret
  * @param permissions - What permissions your client needs access to on the API
